c0981d95274a505cb5d4c75c2671a8b8049d73aa
JSansoP <61268003+JSansoP@users.noreply.github.com>
2022-09-23 11:31:42 +0200

    Comments + clean

--- Practica1/Perceptron.py
+++ Practica1/Perceptron.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 class Perceptron:
     """Perceptron classifier.
 
@@ -18,6 +19,7 @@ class Perceptron:
         Number of misclassifications in every epoch.
 
     """
+
     def __init__(self, eta=0.01, n_iter=10):
         self.eta = eta
         self.n_iter = n_iter
@@ -40,20 +42,30 @@ class Perceptron:
         self : object
 
         """
+        # Initiate weights: one weight for each dimension plus an extra one
+        # for the threshold
         self.w_ = np.zeros(1 + X.shape[1])
+
+        # For each epoch, we do:
         for iter in range(self.n_iter):
+            # For each entry:
             for i, x in enumerate(X):
-                prediction = self.result(x)
+                # We get the prediction of the entry
+                prediction = self.compute(x)
+                # If the prediction is incorrect, we increase the number of errors in this epoch
                 if prediction != y[i]:
                     self.errors_[iter] += 1
+                # We update the weights with the entry, prediction and expected value
                 self.update_weights(x, prediction, y[i])
         return self
 
     def update_weights(self, x, prediction, target):
+        # For each dimension of the entry, we update the weight
         for w in range(x.shape[0]):
-            deltaw = self.eta*(target - prediction)*x[w]
-            self.w_[w+1] += deltaw
-        self.w_[0] += self.eta*(target - prediction)
+            deltaw = self.eta * (target - prediction) * x[w]
+            self.w_[w + 1] += deltaw
+        # Finally we update the weight of the threshold
+        self.w_[0] += self.eta * (target - prediction)
 
     def predict(self, X):
         """Return class label.
@@ -62,10 +74,16 @@ class Perceptron:
             Return a list with classes
         """
         out = []
+        # For each entry in the set, we compute the value and store it in the output list
         for x in X:
-            out.append(self.result(x))
+            out.append(self.compute(x))
         return out
 
-    def result(self, x):
-        z = np.dot(x, self.w_[1:])+self.w_[0]
-        return 1 if z >=0 else -1
+    def activate(self, z):
+        # Step function
+        return 1 if z >= 0 else -1
+
+    def compute(self, x):
+        # Computes weights * entry + threshold weight, and returns
+        z = np.dot(x, self.w_[1:]) + self.w_[0]
+        return self.activate(z)
